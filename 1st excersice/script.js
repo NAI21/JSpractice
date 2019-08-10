5facae47a330575f0c6ab6f80e3cba4a8da175f1
Azamat <pu_mi@bk.ru>
2019-08-10 22:05:18 +0500

    improve 1st execise js code

--- 1st excersice/script.js
+++ 1st excersice/script.js
@@ -1,21 +1,24 @@
 'use strict'
 
-let answer1 = prompt("Ваш бюджет на месяц?");
-let answer2 = prompt("Введите дату в формате YYYY-MM-DD");
-prompt("Введите обязательную статью расходов в этом месяце");
-prompt("Во сколько обойдется?");
-let answer3 = prompt("Введите обязательную статью расходов в этом месяце");
-let answer4 = prompt("Во сколько обойдется?");
-let money = answer1;
-let time = answer2;
+let money = prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
 let appData = {
+    budjet : money,
     timeData: time,
-    expenses: {
-        answer3 : answer4
-    },
-    optionalExpenses: " ",
-    income: " ",
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
     savings: false
 };
-alert("Бюджет на 1 день:" + answer1/30);
-console.log(money);
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a2 = prompt("Во сколько обойдется?"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a4 = prompt("Во сколько обойдется?");
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+
+alert("Бюджет на 1 день:" + appData.budjet/30);
