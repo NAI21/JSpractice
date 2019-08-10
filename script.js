17bf458509ecc5d2dd28b849b55af88d60acb3a8
Azamat <pu_mi@bk.ru>
2019-08-10 17:30:20 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(second);
+
+let person = {
+    name: "Azamat",
+    age: 23,
+    isMarried: false
+};
+
+// console.log(person.name);
+
+// let mas = ['apple.png', 'orange.jpg', 'peach.bpm'];
+// console.log(mas[2]);
+// // alert("Game Over");
+// // let answer = confirm("Are you sure?");
+// // console.log(answer);
+// let answer = +prompt("Are you over 18", "Yes");
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+console.log(++incr,decr--);
+console.log(typeof(52));
+
+let odin = true,
+    dva = true;
+console.log(odin && dva);
+
