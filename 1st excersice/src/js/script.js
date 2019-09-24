1be94fb8585dd601450bcdf1a6436add42fcca90
Azamat <pu_mi@bk.ru>
2019-09-24 18:33:28 +0500

    did 7th exercise

---
+++ 1st excersice/src/js/script.js
@@ -0,0 +1,22 @@
+'use strict'
+let startMesuringBtn = document.getElementById('start'),
+    budgetValue = document.querySelector('.budget-value'),
+    dayBudgetValue = document.querySelector('.daybudget-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalExpensesValue = document.querySelector('.optionalexpenses-value'),
+    monthSavingsValue = document.querySelector('.monthsavings-value'),
+    yearSavingsValue = document.querySelector('.yearsavings-value'),
+    expensesItemInput = document.getElementsByClassName('expenses-item'),
+    btn = document.getElementsByTagName('button'),
+    approveBtn1 = btn[0],
+    approveBtn2 = btn[1],
+    calculateBtn = btn[2],
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('.checksavings'),
+    chooseSum = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+console.log(dayValue);
